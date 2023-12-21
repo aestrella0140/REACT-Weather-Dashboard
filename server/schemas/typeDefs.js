d4cad06b7c1dd29f7971ecb2a482650eb2e0414b
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-12-20 17:55:17 -0700

    working resolvers typedefs and server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,8 +7,22 @@ type User {
     password: String
 }
 
+type Auth {
+    token: ID!
+    user: User
+}
+
 type Query {
-    users: [User]
+    users: [User]!
+    user(userId: ID!): User
+    me: User
+}
+
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    removeUser: User
 }
 `;
 module.exports = typeDefs;
