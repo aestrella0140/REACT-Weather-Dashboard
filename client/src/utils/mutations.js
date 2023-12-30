5585adee131c7443ec6cebb0c321e2dbe4aa26e6
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2023-12-30 16:18:00 -0700

    mutations for user data

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,27 @@
+import { gql } from '@apollo/client';
+
+export const ADD_USER = gql`
+mutation Mutation($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+    addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+      user {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+export const LOGIN_USER = gql`
+mutation Mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
