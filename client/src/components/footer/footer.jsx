e33b8b96c1e8737e0aafe853067ccf0c76dcde66
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-01-04 18:57:55 -0700

    footer component

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// want a link to linkin github need a vision page and contact page about me component
+const footer = () => {
+    return (
+        <footer>
+
+        </footer>
+    );
+};
+
+export default footer;
