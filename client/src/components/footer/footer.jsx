59622de084ccc589a752920683dad189cf6c3ed4
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-01-08 18:48:21 -0700

    footer plugins for anchors

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -3,7 +3,31 @@ import React from "react";
 const footer = () => {
     return (
         <footer>
-
+            <div>
+                <h1>
+                    My Media
+                </h1>
+                <ul>
+                    <li>
+                        GitHub 
+                        <a href="https://github.com/aestrella0140">GitHub</a>
+                    </li>
+                    <li>
+                        LinkedIn 
+                        <a href="https://www.linkedin.com/in/adrian-estrella-7a91b42a2/">LinkedIn</a>
+                    </li>
+                </ul>
+            </div>
+            <div>
+                <h1>
+                    Contact
+                </h1>
+                <ul>
+                    <li>
+                        Email: astarfirstborn@hotmail.com
+                    </li>
+                </ul>
+            </div>
         </footer>
     );
 };
