b51cdf94e4e95fd7add00a0cd32dbe30133fab5b
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-01-16 17:54:23 -0700

    css to navbar

--- client/src/components/header/nav.jsx
+++ client/src/components/header/nav.jsx
@@ -2,52 +2,61 @@ import Auth from '../../utils/auth';
 import { Link } from 'react-router-dom';
 
 function Nav() {
-    
+
     function showNav() {
         if (Auth.loggedIn()) {
             return (
-                <ul className='hd-nav'>
-                    <li>
-                        <Link to='/weatherDashboard'>
-                            Weather Dashboard
-                        </Link>
-                    </li>
-                    <li>
-                        <a href="/" onClick={() => Auth.logout()}>
-                            Logout
-                        </a>
-                    </li>
-                </ul>
+                <div className='nav'>
+                    <ul className='hd-nav'>
+                        <li>
+                            <Link to='/weatherDashboard' className='text'>
+                                Weather Dashboard
+                            </Link>
+                        </li>
+                        <li>
+                            <a href="/" onClick={() => Auth.logout()}>
+                                Logout
+                            </a>
+                        </li>
+                    </ul>
+
+                </div>
             );
         } else {
             return (
-                <ul className='hd-nav'>
-                    <li>
-                        <Link to='/Signup'>
-                            Signup
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to='/Login'>
-                            Login
-                        </Link>
-                    </li>
-                </ul>
+                <div className='nav'>
+                    <ul className='hd-nav'>
+                        <li>
+                            <Link to='/Signup' className='text'>
+                                Signup
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/Login' className='text'>
+                                Login
+                            </Link>
+                        </li>
+                    </ul>
+
+                </div>
             );
         }
     }
 
     return (
         <header>
-            <h1>
-                <Link to='/'>
-                    Weather-news
-                </Link>
-            </h1>
+            <div className='nav'>
+                <h1 >
+                    <Link to='/' className='text'>
+                        Weather-news
+                    </Link>
+                </h1>
 
-            <nav>
-                {showNav()}
-            </nav>
+                <nav>
+                    {showNav()}
+                </nav>
+
+            </div>
         </header>
     );
 }
