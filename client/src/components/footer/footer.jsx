b37173264817f28fcc56bbbf5e4f47dafa0be79f
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2024-04-22 17:43:28 -0700

    start of footer using MDBreact

--- client/src/components/footer/footer.jsx
+++ client/src/components/footer/footer.jsx
@@ -1,32 +1,23 @@
 import React from "react";
+import {
+    MDBFooter,
+    MDBContainer,
+    MDBIcon,
+    MDBInput,
+    MDBCol,
+    MDBRow,
+    MDBBt
+} from 'mdb-react-ui-kit';
 // want a link to linkin github need a vision page and contact page about me component
 const Footer = () => {
     return (
-        <footer>
-            <div className="footer">
-                <h1>
-                    My Media
-                </h1>
-                <ul className="ft-list">
-                    <li> 
-                        <a href="https://github.com/aestrella0140">GitHub</a>
-                    </li>
-                    <li>
-                        <a href="https://www.linkedin.com/in/adrian-estrella-7a91b42a2/">LinkedIn</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="footer">
-                <h1>
-                    Contact
-                </h1>
-                <ul className="ft-list">
-                    <li>
-                        Email: astarfirstborn@hotmail.com
-                    </li>
-                </ul>
-            </div>
-        </footer>
+        <MDBFooter className="text-center" color="white" bgColor="dark">
+            <MDBContainer className="p-4">
+                <section className="mb-4">
+
+                </section>
+            </MDBContainer>
+        </MDBFooter>
     );
 };
 
